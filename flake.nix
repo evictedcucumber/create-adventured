ec6de2a3af1e1de1f929a02c70dd78664a08d865
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-06-01 20:32:38 +0200

    feat: add nix flake for installing packwiz

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    packwiz = {
+      url = "github:evictedcucumber/packwiz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    packwiz,
+    ...
+  }:
+    flake-utils.lib.eachSystem flake-utils.lib.allSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+
+        config.allowUnfree = true;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "create-adventured";
+        packages = [packwiz.packages.${system}.default];
+
+        shellHook = ''
+          echo $SHELL
+        '';
+      };
+    });
+}
